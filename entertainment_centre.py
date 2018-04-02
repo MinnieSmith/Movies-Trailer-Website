2d4866f8ff369176403d159e79805382befe4a7c
Minnie Smith <jacta-alea-est@hotmail.com>
2018-04-02 22:32:42 +0800

    Updated Project

--- entertainment_centre.py
+++ entertainment_centre.py
@@ -4,19 +4,20 @@ import fresh_tomatoes
 # List of all the instances of class Movie
 # which calls the constructor media.Movie() to instantiate movie objects
 pride_and_prejudice = media.Movie("Pride and Prejudice",
-                        "https://i.imgur.com/NRmLNeO.jpg",
-                        "https://www.youtube.com/watch?v=P5MmcT_vcBU")
+                                  "https://i.imgur.com/NRmLNeO.jpg",
+                                  "https://www.youtube.com/watch?v=P5MmcT_vcBU"
+                                  )
 
 midnight_in_paris = media.Movie("Midnight in Paris",
-                     "https://i.imgur.com/ymd30Kn.jpg",
-                     "https://www.youtube.com/watch?v=FAfR8omt-CY")
+                                "https://i.imgur.com/ymd30Kn.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
 
 the_imitation_game = media.Movie("The Imitation Game",
                                  "https://i.imgur.com/MrMmAtM.jpg",
                                  "https://www.youtube.com/watch?v=S5CjKEFb-sM")
 
 amelie = media.Movie("Amelie",
-                     "http://t0.gstatic.com/images?q=tbn:ANd9GcSw6HoCME7bsoGz8NuCtvRu9DZE0bE5pieOzZRwMtheYEkgIVov",
+                     "https://i.imgur.com/6xHyNNp.jpg",
                      "https://www.youtube.com/watch?v=HUECWi5pX7o")
 
 cafe_de_flore = media.Movie("Cafe de Flore",
@@ -26,12 +27,11 @@ okja = media.Movie("Okja",
                    "https://i.imgur.com/oZC54uT.jpg",
                    "https://www.youtube.com/watch?v=AjCebKn4iic")
 
-movies = [pride_and_prejudice, midnight_in_paris, the_imitation_game, amelie, cafe_de_flore, okja]
+movies = [pride_and_prejudice, midnight_in_paris, the_imitation_game,
+          amelie, cafe_de_flore, okja]
 
 fresh_tomatoes.open_movies_page(movies)
-# fresh_tomatoes module was forked and cloned from Udacity through GitHub.
+
 # the open_movies_page function takes in the array movies as an argument
 # and creates a HTML file which allows you to click on the movie poster and
 # watch the trailer.
-
-
--- media.py
+++ media.py
@@ -1,8 +1,15 @@
-class Movie():
-    def __init__(self, movie_title, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+""" The movie class creates a data structure which will store
+information of movie objects and is used to create movie
+instances in the entertainment.py file."""
 
-# Data structure which will store the title, poster images and movie trailer of
-# my favourite movies.
+
+class Movie:
+    # The class constructor which creates space in memory to store
+    # information of instance variables.
+    def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+        # The title of the movie.
+        self.title = movie_title
+        # The string URL for the image of this movie poster.
+        self.poster_image_url = poster_image_url
+        # The string URL for the trailer for this movie.
+        self.trailer_youtube_url = trailer_youtube_url
