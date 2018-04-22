5919d8f9605f7fb169fc99cdcef05f7c6a9b273c
Minnie Smith <jacta-alea-est@hotmail.com>
2018-04-22 16:19:03 +0800

    change class documentation

--- media.py
+++ media.py
@@ -1,15 +1,12 @@
-""" The movie class creates a data structure which will store
-information of movie objects and is used to create movie
-instances in the entertainment.py file."""
-
-
 class Movie:
-    # The class constructor which creates space in memory to store
-    # information of instance variables.
+""" The Movie class creates a data structure which will store
+information of movie objects and is used to create movie
+instances in the entertainment.py file. The class constructor 
+creates space in memory to store information of instance variables."""
     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
-        # The title of the movie.
+        """ The title of the movie, the string URL for the movie poster,
+        and the string URL for the movie trailer is initialised by 
+        instance variables."""
         self.title = movie_title
-        # The string URL for the image of this movie poster.
         self.poster_image_url = poster_image_url
-        # The string URL for the trailer for this movie.
         self.trailer_youtube_url = trailer_youtube_url
